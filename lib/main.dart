599bf6da69dbcf5fc0f9c9d4b9c84dbef1aa4de4
Your Name <you@example.com>
2021-03-18 23:47:26 -0300

    FlatButton and gesture detection. closes #3.

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('left button go pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Right button go pressed');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),
